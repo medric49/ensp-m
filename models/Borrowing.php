d22ae5796c1a26364c39765cc3d551967342fba9
DJEAFEA SONWA MEDRIC BRUEL <medric49@gmail.com>
2018-12-28 11:55:24 +0100

    Crée les models

---
+++ models/Borrowing.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medric
+ * Date: 28/12/18
+ * Time: 11:53
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Borrowing extends ActiveRecord
+{
+
+}
