62c557f742adfdeb0ef0617719cecbce6195ced9
DJEAFEA SONWA MEDRIC BRUEL <medric49@gmail.com>
2018-12-31 12:56:41 +0100

    Fixe le bug du username de l'admin

--- views/administrator/administrators.php
+++ views/administrator/administrators.php
@@ -5,62 +5,74 @@
     <style>
     </style>
 <?php $this->endBlock() ?>
-<div class="container mt-5 mb-5">
-    <div class="row">
-        <?php if (count($administrators) ):?>
-            <div class="col-12 mt-2">
-                <div class="row">
-                    <?php foreach ($administrators as $administrator):?>
-                        <?php
-                        $user = $administrator->user();
-                        ?>
-                        <div class="col-4">
-                            <div class="card">
-                                <!-- Card image -->
-                                <div class="view overlay">
-                                    <img class="card-img-top" src="<?= \app\managers\FileManager::loadAvatar($user,"512") ?>" style="height: 12rem" alt="Card image cap">
-                                    <a href="javascript:void()">
-                                        <div class="mask rgba-white-slight"></div>
-                                    </a>
-                                </div>
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <?php if (count($administrators)): ?>
+                <div class="col-12 mt-2">
+                    <div class="row">
+                        <?php foreach ($administrators as $administrator): ?>
+                            <?php
+                            $user = $administrator->user();
+                            ?>
+                            <div class="col-4 mb-2">
+                                <div class="card">
+                                    <!-- Card image -->
+                                    <div class="view overlay">
+                                        <img class="card-img-top"
+                                             src="<?= \app\managers\FileManager::loadAvatar($user, "512") ?>"
+                                             style="height: 12rem" alt="Card image cap">
+                                        <a href="javascript:void()">
+                                            <div class="mask rgba-white-slight"></div>
+                                        </a>
+                                    </div>
 
-                                <!-- Card content -->
-                                <div class="card-body">
+                                    <!-- Card content -->
+                                    <div class="card-body">
 
-                                    <!-- Title -->
-                                    <h4 class="card-title text-capitalize"><?= $user->name.' '.$user->first_name ?>
-                                        <?php
-                                        if ($user->id == $this->params['user']->id):
+                                        <!-- Title -->
+                                        <h4 class="card-title text-capitalize"><?= $user->name . ' ' . $user->first_name ?>
+                                            <?php
+                                            if ($user->id == $this->params['user']->id):
+                                                ?>
+                                                <span class="text-secondary">(Vous)</span>
+                                            <?php
+                                            endif;
+                                            ?>
+                                        </h4>
+                                        <!-- Text -->
+                                        <p class="card-text">
+                                            <span>Pseudo : </span><span
+                                                    class="blue-text"><?= $administrator->username ?></span>
+                                            <br
+                                            <span>Email : </span><span class="blue-text"><?= $user->email ?></span>
+                                            <br>
+                                            <span>Tel : </span><span class="text-secondary"><?= $user->tel ?></span>
+                                        </p>
+                                        <!-- Button -->
+                                    </div>
+                                    <?php
+                                    if ($administrator->root):
                                         ?>
-                                        <span class="text-secondary">(Vous)</span>
-                                        <?php
-                                        endif;
-                                        ?>
-                                    </h4>
-                                    <!-- Text -->
-                                    <p class="card-text">
-                                        <span>Pseudo : </span><span class="blue-text"><?= $administrator->username ?></span>
-                                        <br
-                                        <span>Email : </span><span class="blue-text"><?= $user->email ?></span>
-                                        <br>
-                                        <span>Tel : </span><span class="text-secondary"><?= $user->tel ?></span>
-                                    </p>
-                                    <!-- Button -->
+                                        <div class="card-footer primary-color text-white text-center">
+                                            Root
+                                        </div>
+                                    <?php
+                                    endif; ?>
                                 </div>
                             </div>
-                        </div>
-                    <?php
-                    endforeach;
-                    ?>
+                        <?php
+                        endforeach;
+                        ?>
 
+                    </div>
                 </div>
-            </div>
-        <?php endif;?>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
 <?php
 if ($this->params['administrator']->root):
-?>
-<a href="<?= Yii::getAlias("@administrator.new_administrator") ?>" class="btn btn-primary btn-add"><i class="fas fa-plus"></i></a>
+    ?>
+    <a href="<?= Yii::getAlias("@administrator.new_administrator") ?>" class="btn btn-primary btn-add"><i
+                class="fas fa-plus"></i></a>
 <?php
 endif;
